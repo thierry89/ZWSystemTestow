b10e89dbe75983e5a9fe6c7457bcb20154c20d11
Viniolli <viniolli@gmail.com>
2012-02-26 12:26:19 +0100

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZWSystemTestow::Application.config.secret_token = '7c2ce5810ae216348290c54e25b0b7b5813fb5ef822768c607093af6a3cc1b4f6c733c8ce8c7871280126166a30406173386848fbe79b323be594649ff742044'
